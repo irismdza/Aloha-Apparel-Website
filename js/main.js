6aef8bb56d64639afcad2f9450bcc02099032055
Iris <mdza.iris@gmail.com>
2017-01-20 18:39:21 -0800

    Added navigation menu

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// $(function() {
+//     $('button').on('click', function() {
+//         if('input') === valid email function {
+//         confirm("You have been subscribed!")
+//         }
+//         else {
+//             confirm("Please enter a valid email")
+//         };
+//     });
+// });
