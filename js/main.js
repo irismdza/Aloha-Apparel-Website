d1be36599f120430887c1622ed52aece2b3c4ff3
Iris <mdza.iris@gmail.com>
2017-01-23 06:15:11 -0800

    Implemented navigation smooth scrolling and added Flickity components

--- js/main.js
+++ js/main.js
@@ -1,10 +1,36 @@
-// $(function() {
-//     $('button').on('click', function() {
-//         if('input') === valid email function {
-//         confirm("You have been subscribed!")
-//         }
-//         else {
-//             confirm("Please enter a valid email")
-//         };
-//     });
-// });
+$(function () {
+
+    $('a[href*="#"]:not([href="#"])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+
+    $('.main-carousel').flickity({
+        // options
+        cellAlign: 'left',
+        contain: true
+    });
+
+
+    // $(function() {
+    //     $('button').on('click', function() {
+    //         if('input') === valid email function {
+    //         confirm("You have been subscribed!")
+    //         }
+    //         else {
+    //             confirm("Please enter a valid email")
+    //         };
+    //     });
+    // });
+
+
+
+});
